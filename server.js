f7264665e64b263d4eee9f082bab0976cbabac76
Alexis Merino <alexismerino49@gmail.com>
2024-04-08 14:30:50 -0500

    added middlewear and structure to add html and API route calls

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express =require("express");// importing express 
-const path = require("path");// importing path to work with directories and file paths
 const db = require ("./db/db.json");// importing db.json file where the user input will be stored.
 
-const app = express();// express is turned into app const for easier calls
-const PORT = process.env.port || 3001;// the port which can be used to check if the backend is working on insomia
+// dependencies
+const path = require("path");// importing path to work with directories and file paths
+const fs = require("fs"); // importing file system module
+
+
+// envriorment variable port
+const PORT = process.env.port || 3001;
+
+// express is turned into app const for easier calls
+const app = express();
+// middleware for parsing JSON and urlcoded form data
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// express creates a path for all the files in the public folder.
+app.use (express.static("public"));
+
+// Routes
+
+//HTML
+
+//API
 
